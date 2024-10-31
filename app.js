3e070d91c490a98ff1db02d6bb3695ab723159fe
Brendan <brendanlawless@hotmail.com>
2024-10-31 13:49:30 +0000

    fix: add absolute path to route handlers

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+import { fileURLToPath } from "url"
+import { dirname } from "path"
 import express from "express"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
-app.get("/", (req, res) => res.sendFile("./index.html"))
-app.get("/contact", (req, res) => res.sendFile("./contact.html"))
-app.get("/about", (req, res) => res.sendFile("./about.html"))
-app.get("/*", (req, res) => res.sendFile("./404.html"))
+const options = {root: __dirname}
+
+app.get("/", (req, res) => res.sendFile("index.html", options))
+app.get("/contact", (req, res) => res.sendFile("contact.html", options))
+app.get("/about", (req, res) => res.sendFile("about.html", options))
+app.get("/*", (req, res) => res.sendFile("404.html", options))
 
 app.listen(PORT,  () => console.log(`listening on http://localhost:${PORT}`))
